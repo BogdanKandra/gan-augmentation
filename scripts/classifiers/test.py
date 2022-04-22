9a03b0665750316af56c7c48d704a4c5a1a588ee
Bogdan-Alexandru Kandra <bogdan-alexandru.kandra@1and1.ro>
2022-04-22 18:41:12 +0300

    Added test concrete classifier class

--- scripts/classifiers/test.py
+++ scripts/classifiers/test.py
@@ -1,32 +1,28 @@
-import matplotlib.pyplot as plt
+from scripts.classifiers.fashion_mnist_classifier import FashionMNISTClassifier
 import numpy as np
-from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.utils import to_categorical
 
 
-(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+class TestClassifier(FashionMNISTClassifier):
+    """ Test class representing a concrete implementation of the base classifier """
+    def __init__(self):
+        """ This just calls the base class' constructor """
+        super().__init__()
+
+    def preprocess_dataset(self) -> None:
+        """ This reshapes the dataset and encodes the labels """
+        self.y_train = to_categorical(self.y_train)
+        self.y_test = to_categorical(self.y_test)
+
+        x_temp_train = self.X_train.reshape((*self.X_train.shape, 1)).astype(float)
+        x_temp_test = self.X_test.reshape((*self.X_test.shape, 1)).astype(float)
+        self.X_train = x_temp_train / 255.0
+        self.X_test = x_temp_test / 255.0
 
 
-def show_dataset_information():
-    print('Train Set Shape: X_train.shape={}, y_train.shape={}'.format(X_train.shape, y_train.shape))
-    print('Test Set Shape: X_test.shape={}, y_test.shape={}'.format(X_test.shape, y_test.shape))
-
-
-def show_dataset_sample():
-    for i in range(25):
-        plt.subplot(5, 5, i + 1)
-        plt.imshow(X_train[i], cmap=plt.get_cmap('gray'))
-
-    plt.show()
-
-
-X_train = X_train.reshape((*X_train.shape, 1))
-X_test = X_test.reshape((*X_test.shape, 1))
-
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-
-X_train_norm = X_train.astype(np.float)
-X_test_norm = X_test_astype(np.float)
-X_train_norm = X_train_norm / 255.0
-X_test_norm = X_test_norm / 255.0
+clf = TestClassifier()
+clf.display_dataset_information()
+clf.display_dataset_sample(3)
+clf.display_model()
+clf.preprocess_dataset()
+clf.display_dataset_information()
