c8fa152a910bec6adea172f1ea50df7b392171dc
Bogdan-Alexandru Kandra <bogdan-alexandru.kandra@1and1.ro>
2022-04-23 22:24:22 +0300

    Added paths and training constants

--- scripts/config.py
+++ scripts/config.py
@@ -3,8 +3,17 @@ import os
 
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
 
+PROJECT_PATH = os.getcwd()
+while os.path.basename(PROJECT_PATH) != 'gan-augmentation':
+    PROJECT_PATH = os.path.dirname(PROJECT_PATH)
+RESULTS_PATH = os.path.join(PROJECT_PATH, 'results')
+CNN_RESULTS_PATH = os.path.join(RESULTS_PATH, 'cnn-training')
+GAN_RESULTS_PATH = os.path.join(RESULTS_PATH, 'gan-training')
+
 TRAIN_SET_PERCENTAGE = 0.85
 VALID_SET_PERCENTAGE = 0.15
 RANDOM_STATE = 29
-NUM_EPOCHS = 10
-BATCH_SIZE = 32
+NUM_EPOCHS_WEAK = 10
+NUM_EPOCHS_STRONG = 50
+BATCH_SIZE_WEAK = 32
+BATCH_SIZE_STRONG = 16
