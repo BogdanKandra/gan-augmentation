944b21a50ba4379df57473ceca5fb2eb10939764
Bogdan-Alexandru Kandra <bogdan-alexandru.kandra@1and1.ro>
2022-04-27 17:50:45 +0300

    Added paths for models dirs

--- scripts/config.py
+++ scripts/config.py
@@ -6,9 +6,12 @@ LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
 PROJECT_PATH = os.getcwd()
 while os.path.basename(PROJECT_PATH) != 'gan-augmentation':
     PROJECT_PATH = os.path.dirname(PROJECT_PATH)
+MODELS_PATH = os.path.join(PROJECT_PATH, 'models')
+CNN_MODELS_PATH = os.path.join(MODELS_PATH, 'cnn')
+GAN_MODELS_PATH = os.path.join(MODELS_PATH, 'gan')
 RESULTS_PATH = os.path.join(PROJECT_PATH, 'results')
-CNN_RESULTS_PATH = os.path.join(RESULTS_PATH, 'cnn-training')
-GAN_RESULTS_PATH = os.path.join(RESULTS_PATH, 'gan-training')
+CNN_RESULTS_PATH = os.path.join(RESULTS_PATH, 'cnn')
+GAN_RESULTS_PATH = os.path.join(RESULTS_PATH, 'gan')
 
 TRAIN_SET_PERCENTAGE = 0.85
 VALID_SET_PERCENTAGE = 0.15
