5ca07ba265fe283b7f109effa7faa885bb6d3e72
BogdanKandra <bogdan.kandra@gmail.com>
2024-09-21 00:36:44 +0300

    Implemented DCGAN

--- scripts/models/dcgan.py
+++ scripts/models/dcgan.py
@@ -1,5 +1,3 @@
-from math import prod
-
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -10,10 +8,10 @@ from scripts.config import GeneratorDataset
 
 class Generator(nn.Module):
     def __init__(self, dataset: GeneratorDataset) -> None:
-        """ Class representing the generator part from the vanilla GAN. It consists of 4 blocks, each containing a
-        Linear layer, BatchNorm layer, and ReLU activation. The output layer is a Linear layer with sigmoid activation.
-        Since this is a conditional GAN, the input consists of a noise vector, concatenated with the desired one-hot
-        encoded label.
+        """ Class representing the generator part from the DCGAN. It consists of 4 blocks, each containing a
+        Transposed Convolution layer, BatchNorm layer (except in the final layer), and ReLU activation. The output
+        layer is a Transposed Convolution layer with tanh activation. Since this is a conditional GAN, the input
+        consists of a noise vector, concatenated with the desired one-hot encoded label.
 
         Model hyperparameters:
             z_dim (int): the number of features in the latent space / dimension of the noise vector
@@ -28,27 +26,30 @@ class Generator(nn.Module):
         match dataset:
             case GeneratorDataset.FASHION_MNIST:
                 self.z_dim = 64
-                self.h_dim = 128
-                self.input_dim = self.z_dim + len(config.FASHION_MNIST_CLASS_LABELS)
-                self.out_features = prod(config.FASHION_MNIST_SHAPE)
-                self.out_shape = config.FASHION_MNIST_SHAPE
+                self.h_dim = 64
+                self.in_channels = self.z_dim + len(config.FASHION_MNIST_CLASS_LABELS)
+                self.out_channels = config.FASHION_MNIST_SHAPE[0]
+                self.kernel_sizes = [3, 4, 3, 4]
+                self.strides = [2, 1, 2, 2]
             case GeneratorDataset.CIFAR_10:
                 self.z_dim = 128
-                self.h_dim = 512
-                self.input_dim = self.z_dim + len(config.CIFAR_10_CLASS_LABELS)
-                self.out_features = prod(config.CIFAR_10_SHAPE)
-                self.out_shape = config.CIFAR_10_SHAPE
+                self.h_dim = 128
+                self.in_channels = self.z_dim + len(config.CIFAR_10_CLASS_LABELS)
+                self.out_channels = config.CIFAR_10_SHAPE[0]
+                self.kernel_sizes = [3, 3, 3, 4]
+                self.strides = [2, 2, 2, 2]
             case _:
                 raise ValueError("Unsupported dataset type")
 
         self.generator = nn.Sequential(
-            self._generator_block(self.input_dim, self.h_dim),
-            self._generator_block(self.h_dim, self.h_dim * 2),
-            self._generator_block(self.h_dim * 2, self.h_dim * 4),
-            self._generator_block(self.h_dim * 4, self.h_dim * 8),
-            nn.Linear(self.h_dim * 8, self.out_features),
-            nn.Sigmoid(),
-            nn.Unflatten(1, self.out_shape)
+            self._generator_block(self.in_channels, self.h_dim * 4,
+                                  kernel_size=self.kernel_sizes[0], stride=self.strides[0]),
+            self._generator_block(self.h_dim * 4, self.h_dim * 2,
+                                  kernel_size=self.kernel_sizes[1], stride=self.strides[1]),
+            self._generator_block(self.h_dim * 2, self.h_dim,
+                                  kernel_size=self.kernel_sizes[2], stride=self.strides[2]),
+            self._generator_block(self.h_dim, self.out_channels,
+                                  kernel_size=self.kernel_sizes[3], stride=self.strides[3], final_layer=True)
         )
 
     def forward(self, z: Tensor, labels: Tensor) -> Tensor:
@@ -57,18 +58,29 @@ class Generator(nn.Module):
         Fashion-MNIST: (64 + 10) -> (128) -> (256) -> (512) -> (1024) -> (1*28*28) -> (1, 28, 28)
         CIFAR-10: (128 + 10) -> (512) -> (1024) -> (2048) -> (4096) -> (3*32*32) -> (3, 32, 32)
         """
-        generator_input = torch.cat((z, labels), dim=1)
-        x = self.generator(generator_input)
+        z_and_labels = torch.cat((z, labels), dim=1)
+        z_and_labels = z_and_labels.view(z_and_labels.shape[0], self.in_channels, 1, 1)
 
-        return x
+        return self.generator(z_and_labels)
 
-    def _generator_block(self, in_features: int, out_features: int) -> None:
+    def _generator_block(self,
+                         in_channels: int,
+                         out_channels: int,
+                         kernel_size: int = 3,
+                         stride: int = 2,
+                         final_layer: bool = False) -> None:
         """ Builds a block of the generator's model. """
-        return nn.Sequential(
-            nn.Linear(in_features, out_features),
-            nn.BatchNorm1d(out_features),
-            nn.ReLU(),
-        )
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
 
 
 class Discriminator(nn.Module):
@@ -89,34 +101,27 @@ class Discriminator(nn.Module):
         # Set hyperparameters and compute the output features based on the specified dataset
         match dataset:
             case GeneratorDataset.FASHION_MNIST:
-                self.in_features = prod(config.FASHION_MNIST_SHAPE)
+                self.in_shape = config.FASHION_MNIST_SHAPE
                 self.h_dim = 512
-                self.input_dim = self.in_features + len(config.FASHION_MNIST_CLASS_LABELS)
-                self.final_hidden_features = self.h_dim // 4
-                self.discriminator_blocks = [
-                    self._discriminator_block(in_features, out_features) for (in_features, out_features) in
-                    [(self.input_dim, self.h_dim),
-                     (self.h_dim, self.h_dim // 2),
-                     (self.h_dim // 2, self.h_dim // 4)]
-                ]
+                self.in_channels = self.in_shape[0] + len(config.FASHION_MNIST_CLASS_LABELS)
+                self.kernel_sizes = [4, 4, 4]
+                self.strides = [2, 2, 2]
             case GeneratorDataset.CIFAR_10:
-                self.in_features = prod(config.CIFAR_10_SHAPE)
-                self.h_dim = 2048
-                self.input_dim = self.in_features + len(config.CIFAR_10_CLASS_LABELS)
-                self.final_hidden_features = self.h_dim // 8
-                self.discriminator_blocks = [
-                    self._discriminator_block(in_features, out_features) for (in_features, out_features) in
-                    [(self.input_dim, self.h_dim),
-                     (self.h_dim, self.h_dim // 2),
-                     (self.h_dim // 2, self.h_dim // 4),
-                     (self.h_dim // 4, self.h_dim // 8)]
-                ]
+                self.in_shape = config.CIFAR_10_SHAPE
+                self.h_dim = 512
+                self.in_channels = self.in_shape[0] + len(config.CIFAR_10_CLASS_LABELS)
+                self.kernel_sizes = [5, 5, 4]
+                self.strides = [2, 2, 2]
             case _:
                 raise ValueError("Unsupported dataset type")
 
         self.discriminator = nn.Sequential(
-            *self.discriminator_blocks,
-            nn.Linear(self.final_hidden_features, 1)
+            self._discriminator_block(self.in_channels, self.h_dim,
+                                      kernel_size=self.kernel_sizes[0], stride=self.strides[0]),
+            self._discriminator_block(self.h_dim, self.h_dim * 2,
+                                      kernel_size=self.kernel_sizes[1], stride=self.strides[1]),
+            self._discriminator_block(self.h_dim * 2, 1,
+                                      kernel_size=self.kernel_sizes[2], stride=self.strides[2], final_layer=True)
         )
 
     def forward(self, x: Tensor, labels: Tensor) -> Tensor:
@@ -125,15 +130,25 @@ class Discriminator(nn.Module):
         Fashion-MNIST: (1, 28, 28) -> (1*28*28) -> (784 + 10) -> (512) -> (256) -> (128) -> (1)
         CIFAR-10: (3, 32, 32) -> (3*32*32) -> (3072 + 10) -> (2048) -> (1024) -> (512) -> (256) -> (1)
         """
-        x_flattened = x.view(x.shape[0], -1)
-        discriminator_input = torch.cat((x_flattened, labels), dim=1)
-        x = self.discriminator(discriminator_input)
+        x_and_labels = torch.cat((x, labels), dim=1)
+        x = self.discriminator(x_and_labels)
 
-        return x
+        return x.view(len(x_and_labels), -1)
 
-    def _discriminator_block(self, in_features: int, out_features: int) -> None:
+    def _discriminator_block(self,
+                             in_channels: int,
+                             out_channels: int,
+                             kernel_size: int = 4,
+                             stride: int = 2,
+                             final_layer: bool = False) -> None:
         """ Builds a block of the discriminator's model. """
-        return nn.Sequential(
-            nn.Linear(in_features, out_features),
-            nn.LeakyReLU(negative_slope=0.2),
-        )
+        if not final_layer:
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride),
+                nn.BatchNorm2d(out_channels),
+                nn.LeakyReLU(negative_slope=0.2)
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+            )
